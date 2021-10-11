655d51b3ff4e1ba48ef0520a3c37629b19368cc6
rodrigogrand <rodrigogrande@programmer.net>
2021-10-10 21:55:02 -0600

    New Features

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ console.log("segundo commit")
 // I need to add this line
 // I need to add also this line
 
-// This is your age
+// This is your age, you need to fill out the prompt, and your age is going to display your actual age.
 let yourAge = prompt("Enter your birth year?");
 let actualYear = 2021;
 let yourActualAge = ''
