9881a5ff343ac63c057f2c2310af13dc473d1dd2
rodrigogrand <rodrigogrande@programmer.net>
2021-10-10 21:48:04 -0600

    New Features

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 console.log("segundo commit")
 // I need to add this line
 // I need to add also this line
+
+// This is your age
+let yourAge = prompt("Enter your birth year?");
+let actualYear = 2021;
+let yourActualAge = ''
+if (yourAge) {
+    document.write("Your age is " + (actualYear - yourAge));
+}
